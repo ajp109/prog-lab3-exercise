5e5907a4ac07a7a3231afe5d459fae09544d0867
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-04 16:57:50 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "mbed.h"
 
-int main() {
-    while (true) {
-        sleep();
-    }
+// Write your function here
+
+int main()
+{
+    // Initialise the digital pins D2 to D5 as outputs
+    DigitalOut blue(D2);
+    DigitalOut red(D3);
+    DigitalOut yellow(D4);
+    DigitalOut green(D5);
+    
+    // Call your function here
+
+    // Do nothing, forever, when the program is complete
+    while (true) sleep();
 }
